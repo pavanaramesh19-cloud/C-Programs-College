9a625068c9d58249f7a4447526c1e7dbc86366de
pavanaramesh19-cloud <pavanaramesh19@gmail.com>
2026-02-06 16:31:47 +0530

    Delete prog18.c

--- prog18.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main() {
-   int i = 6;
-    printf("i++ = %d\n", i++);//Post-increment
-    printf("++i = %d\n", ++i);//Pre-increment
-    printf("i-- = %d\n", i--);//Post-decrement
-    printf("--i = %d\n", --i);//Pre-decrement
-    return 0;
-}
